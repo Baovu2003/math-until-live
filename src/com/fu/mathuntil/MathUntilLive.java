e82ce87f18db5c9d3e1dc08ada8688a4802f6da1
doit-now <vuluongbao.qc2003@gmail.com>
2023-06-24 20:49:34 +0700

    Lan dau dua code len sever

---
+++ src/com/fu/mathuntil/MathUntilLive.java
@@ -0,0 +1,11 @@
+
+package com.fu.mathuntil;
+
+public class MathUntilLive {
+
+    public static void main(String[] args) {
+        long result = MathUntil.getFactorial(5);
+        System.out.println("expected: 5! -120; actual: "+result);
+    }
+    
+}
